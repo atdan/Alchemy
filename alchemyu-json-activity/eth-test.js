ef481a8594d5656f836e040660706814abef2f1c
Daniel Atuma <atumadann@gmail.com>
2023-12-19 14:16:58 +0100

    update alchemy get total balance

--- alchemyu-json-activity/eth-test.js
+++ alchemyu-json-activity/eth-test.js
@@ -2,6 +2,16 @@ const { assert, util: { inspect }} = require('chai');
 const getBlockNumber = require('./getBlockNumber');
 const getBalance = require('./getBalance');
 const getNonce = require('./getNonce');
+const getTotalTransactions = require('./getTotalTransactions');
+const getTotalBalance = require('./getTotalBalance');
+
+const addresses = [
+    '0x830389b854770e9102eb957379c6b70da4283d60',
+    '0xef0613ab211cfb5eeb5a160b65303d6e927f3f85',
+    '0x5311fce951684e46cefd804704a06c5133030dff',
+    '0xe01c0bdc8f2a8a6220a4bed665ceeb1d2c716bcb',
+    '0xf6c68965cdc903164284b482ef5dfdb640d9e0de'
+];
 
 describe('getBlockNumber', function () {
     it('should get the current block number', async () => {
@@ -34,5 +44,30 @@ describe('getNonce', () => {
         const parsed = parseInt(nonce);
         assert(!isNaN(parsed), `We expected you to return a nonce, here is what you returned: ${inspect(nonce)}`);
         assert.isAbove(parsed, 1015);
+    });
+});
+
+describe('getTotalTransactions', () => {
+    it('should work for an empty block', async () => {
+        const numTx = await getTotalTransactions('0x' + (12379).toString(16));
+        const parsed = parseInt(numTx);
+        assert(!isNaN(parsed), `We expected you to return a transactions count, here is what you returned: ${inspect(numTx)}`);
+        assert.equal(parsed, 0);
+    });
+
+    it('should work for a recent block', async () => {
+        const numTx = await getTotalTransactions('0x' + (16642379).toString(16));
+        const parsed = parseInt(numTx);
+        assert(!isNaN(parsed), `We expected you to return a transactions count, here is what you returned: ${inspect(numTx)}`);
+        assert.equal(parsed, 206);
+    });
+});
+
+describe('getTotalBalance', () => {
+    it('should find the total balance of all the addresses', async () => {
+        const totalBalance = await getTotalBalance(addresses);
+        const parsed = parseInt(totalBalance);
+        assert(!isNaN(parsed), `We expected you to return a total balance, here is what you returned: ${inspect(totalBalance)}`);
+        assert.isAtLeast(parsed, 250000000000000000);
     });
 });
