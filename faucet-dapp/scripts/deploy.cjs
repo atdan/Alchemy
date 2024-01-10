4c83bce867d795fd0fa11a92b0d522e85731bd3d
Daniel Atuma <atumadann@gmail.com>
2024-01-10 08:51:21 +0100

    add faucet dapp and deploy to gorlei network

---
+++ faucet-dapp/scripts/deploy.cjs
@@ -0,0 +1,33 @@
+const { ethers, JsonRpcProvider } = require('ethers');
+require('dotenv').config();
+
+async function main() {
+
+  const url = process.env.GOERLI_URL;
+
+  let artifacts = await hre.artifacts.readArtifact("Faucet");
+
+  const provider = new JsonRpcProvider(url);
+
+  let privateKey = process.env.PRIVATE_KEY;
+
+  let wallet = new ethers.Wallet(privateKey, provider.provider);
+
+  // Create an instance of a Faucet Factory
+  let factory = new ethers.ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
+
+  let faucet = await factory.deploy();
+
+  await faucet.waitForDeployment();
+
+    console.log("Faucet address:", await faucet.getAddress());
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+
+//Faucet deploy address: 0xA8e4b84a3572820Ee8a10Fd9408a4c9dDbD86419
